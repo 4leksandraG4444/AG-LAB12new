9d223ce1140be11b5a5159c028a4c5fd47a5f72d
4leksandraG4444 <aleksandra.alicja.gora@gmail.com>
2025-06-17 15:50:59 +0200

    Update main.js

--- laboratorium12/src/main.js
+++ laboratorium12/src/main.js
@@ -14,9 +14,6 @@ const addArticleForm = document.getElementById('addArticle')
 document.querySelector('#app').innerHTML = `
   <div>
     <h1 class="text-2xl font-bold text-primary">Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button" class="bg-blue-300 p-4"></button>
-    </div>
   </div>
 `
 setupCounter(document.querySelector('#counter'))
