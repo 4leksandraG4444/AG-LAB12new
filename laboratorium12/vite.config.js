0e7d857853e86c428ff81cee4ef07ebd6c415ada
4leksandraG4444 <aleksandra.alicja.gora@gmail.com>
2025-06-17 16:05:39 +0200

    Update vite.config.js

--- laboratorium12/vite.config.js
+++ laboratorium12/vite.config.js
@@ -1,23 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
-  plugins: [react()],
-  base: '/AG-LAB12new/',
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      external: [
-        '@supabase/supabase-js',
-        'date-fns'
-      ],
-    }
-  },
-  optimizeDeps: {
-    include: [
-      '@supabase/supabase-js',
-      'date-fns'
-    ]
-  }
-})
+  plugins: [
+    tailwindcss()
+  ],
+});
